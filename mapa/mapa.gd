074374ea70c6abc8eddc51ee902b764ca72638cf
Max Żurawski <max.zurawski@gmail.com>
2026-07-11 22:17:52 +0200

    podpięcie RadarMapy do mapy

--- mapa/mapa.gd
+++ mapa/mapa.gd
@@ -1,6 +1,8 @@
 extends Control
 
 @onready var radar_map = $RadarMap
+signal sector_changed(sector_name: String)
+signal position_changed(map_position: Vector2)
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +13,6 @@ func _ready() -> void:
 	radar_map.path_finished.connect(
 		_on_radar_path_finished
 	)
-
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -28,6 +29,13 @@ func _on_radar_sector_changed(sector_name: String) -> void:
 	#     "SIGNAL POSITION: " + sector_name
 	# )
 
-
 func _on_radar_path_finished() -> void:
 	print("Obiekt zakończył trasę.")
+
+
+func _on_radar_map_position_changed(map_position: Vector2) -> void:
+	position_changed.emit(map_position)
+
+
+func _on_radar_map_sector_changed(sector_name: String) -> void:
+	sector_changed.emit(sector_name)
